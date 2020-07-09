3251749d46f9b4b651929cb4adf97bdc8f64f18b
Allan Carvalho <allandcarv@gmail.com>
2020-07-09 16:39:33 +0100

    Section 5

--- components/Section5/index.tsx
+++ components/Section5/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-import { StyledSection } from './styles';
+import { StyledSection, StyledTable } from './styles';
+
+import icone01 from '../../assets/images/icone1.svg';
+import icone02 from '../../assets/images/icone2.svg';
+import icone03 from '../../assets/images/icone3.svg';
+import icone04 from '../../assets/images/icone4.svg';
+import icone05 from '../../assets/images/icone5.svg';
+import icone07 from '../../assets/images/icone7.svg';
+import icone09 from '../../assets/images/icone9.svg';
+import icone10 from '../../assets/images/icone10.svg';
 
 interface SectionProps {
   id: string;
@@ -8,7 +17,51 @@ interface SectionProps {
 
 const Section5: React.FC<SectionProps> = ({ id }) => (
   <StyledSection id={id}>
-    <h1>Section5</h1>
+    <h1>Vantagens</h1>
+    <StyledTable cellPadding="0" cellSpacing="0" width="100%">
+      <tbody>
+        <tr>
+          <td>
+            <img src={icone01} alt="Preço Competitivo" />
+            <span>Preço competitivo</span>
+          </td>
+          <td>
+            <img src={icone02} alt="Especialistas em Moda" />
+            <span>Especialistas em Moda</span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src={icone03} alt="Rápida Implementação" />
+            <span>Rápida Implementação</span>
+          </td>
+          <td>
+            <img src={icone04} alt="Servidores e Manutenção Assegurados" />
+            <span>Servidores e Manutenção Assegurados</span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src={icone05} alt="Apoio Permanente" />
+            <span>Apoio Permanente</span>
+          </td>
+          <td>
+            <img src={icone10} alt="Aumento de Vendas" />
+            <span>Aumento de Vendas</span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src={icone07} alt="UX/UI Design" />
+            <span>UX/UI Design</span>
+          </td>
+          <td>
+            <img src={icone09} alt="Apoio ao Cliente" />
+            <span>Apoio ao cliente</span>
+          </td>
+        </tr>
+      </tbody>
+    </StyledTable>
   </StyledSection>
 );
 
--- components/Section5/styles.ts
+++ components/Section5/styles.ts
@@ -1,5 +1,64 @@
 import styled from 'styled-components';
 
 export const StyledSection = styled.section`
+  padding: 70px 0;
   background-color: #dedede;
+
+  h1 {
+    margin: 0 auto;
+    width: 66.66%;
+
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 65px;
+    line-height: 70px;
+    font-weight: 600;
+    text-transform: uppercase;
+    color: #000;
+    letter-spacing: 4px;
+    margin-bottom: 30px;
+  }
+`;
+
+export const StyledTable = styled.table`
+  margin: 0 auto;
+  width: 66.66%;
+
+  tr {
+    td {
+      position: relative;
+      padding: 20px;
+      border-top: 1px solid #000;
+
+      &:first-of-type {
+        border-right: 1px solid #000;
+      }
+
+      img {
+        width: 56px;
+        height: auto;
+        margin-right: 25px;
+      }
+
+      span {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        display: inline-flex;
+        align-items: center;
+        font-family: 'Source Sans Pro', sans-serif;
+        font-weight: normal;
+        font-size: 14px;
+        color: #000;
+        line-height: 140%;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+      }
+    }
+
+    &:last-of-type {
+      td {
+        border-bottom: 1px solid #000;
+      }
+    }
+  }
 `;
