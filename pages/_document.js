179c2a9d3c3ff8998a709262b378ae17ea2977f7
Allan Carvalho <allandcarv@gmail.com>
2020-07-10 08:53:45 +0100

    Fix Styled Component Behaviour

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,30 @@
-import Document, { Head, Main, NextScript } from "next/document";
-import flush from "styled-jsx/server";
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage();
-    const styles = flush();
-    return { html, head, errorHtml, chunks, styles };
-  }
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-  render() {
-    return (
-      <html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 }
