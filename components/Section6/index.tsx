4e66f9973e6f4ca5b265acc55b284aa4352c1074
Allan Carvalho <allandcarv@gmail.com>
2020-07-12 17:02:18 +0100

    Section 6

--- components/Section6/index.tsx
+++ components/Section6/index.tsx
@@ -1,15 +1,149 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SwiperCore, { Navigation, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { MdArrowBack, MdArrowForward } from 'react-icons/md';
 
-import { StyledSection } from './styles';
+import { StyledSection, Container } from './styles';
+
+import aboutUs from '../../assets/images/about-us_01_mob.jpg';
+
+SwiperCore.use([Navigation, Autoplay]);
 
 interface SectionProps {
   id: string;
 }
 
-const Section6: React.FC<SectionProps> = ({ id }) => (
-  <StyledSection id={id}>
-    <h1>Section6</h1>
-  </StyledSection>
-);
+interface ClientProps {
+  name: string;
+  role: string;
+  comment: string;
+}
+
+interface CurrentClientProps {
+  name: string | null;
+  role: string | null;
+}
+
+const Section6: React.FC<SectionProps> = ({ id }) => {
+  const clientReviews: ClientProps[] = [
+    {
+      name: 'Paulo Campos',
+      role: 'Diretor Geral Patachou',
+      comment:
+        'É um prazer trabalhar com a Minty, equipa jovem e disciplinar que só conhece o caminho do sucesso.',
+    },
+    {
+      name: 'Adriana Oliveira',
+      role: 'CEO KAOÂ',
+      comment: 'Equipa incrível com resultados surpreendentes.',
+    },
+    {
+      name: 'Vitor Costa',
+      role: 'Creative Director NOBRAND',
+      comment:
+        'A MINTY Lab facilita todo o Business to Business de uma forma refrescante, criative e eficiente.',
+    },
+    {
+      name: 'Pedro Santana',
+      role: 'Director de Marketing e Comunicação Ambitious',
+      comment:
+        'Uma Equipa Muito Competente em várias áreas e uma extensão da equipa Ambitious. São um parceiro essencial para alcançarmos os nossos objetivos.',
+    },
+    {
+      name: 'Ivânia and Carina Santos',
+      role: 'Representates da Rufel',
+      comment:
+        'A MINTY Lab foi fundamenta na expansão da marca para o mercado online internacional. Tornaram possível que os produtos RUFEL chegassem a todo o planeta com facilidade e sofisticação.',
+    },
+  ];
+
+  const [currentClient, setCurrentClient] = useState<CurrentClientProps | null>(
+    {
+      name: clientReviews[0].name,
+      role: clientReviews[0].role,
+    },
+  );
+
+  const onSlideChange = (swiper: Swiper): void => {
+    // @ts-ignore: Unreachable code error
+    const { activeIndex } = swiper;
+
+    if (activeIndex === undefined || activeIndex === null) return;
+
+    setCurrentClient({
+      name: clientReviews[activeIndex].name,
+      role: clientReviews[activeIndex].role,
+    });
+  };
+
+  return (
+    <StyledSection id={id}>
+      <h1>Sobre Nós</h1>
+      <article>
+        <section>
+          <p>
+            Começámos a nossa viagem como marketplace de moda -
+            <a
+              href="https://mintysquare.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              MINTYSQUARE.COM
+            </a>
+            &nbsp; - com o propósito de encurtar a distância entre clientes e
+            designers, reunindo num único espaço as coleções de mais de 200
+            marcas.
+          </p>
+          <p>
+            Combinando a expertise e o know-how adquiridos com o marketplace,
+            desenvolvemos um produto tecnológico capaz de oferecer todos os
+            recursos necessários ao sucesso online de marcas que desejam
+            lançar-se ou simplesmente crescer online.
+          </p>
+        </section>
+        <img src={aboutUs} alt="Sobre Nós" title="Sobre Nós" />
+      </article>
+      <Container>
+        <p>Review dos Clientes</p>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          centeredSlides
+          autoplay={{ delay: 5000 }}
+          navigation={{
+            nextEl: 'footer .swiper-button-next',
+            prevEl: 'footer .swiper-button-prev',
+          }}
+          onSlideChange={swiper => onSlideChange(swiper)}
+        >
+          {clientReviews.map(client => (
+            <SwiperSlide key={client.role}>
+              <p>
+                <strong>{`"${client.comment}`}</strong>
+              </p>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <footer>
+          {currentClient && (
+            <article>
+              <p>{currentClient.name}</p>
+              <p>{currentClient.role}</p>
+            </article>
+          )}
+
+          <div className="actions">
+            <button type="button" className="swiper-button-prev">
+              <MdArrowBack size={90} color="#fff" />
+            </button>
+            <button type="button" className="swiper-button-next">
+              <MdArrowForward size={90} color="#fff" />
+            </button>
+          </div>
+        </footer>
+      </Container>
+    </StyledSection>
+  );
+};
 
 export default Section6;
--- components/Section6/styles.ts
+++ components/Section6/styles.ts
@@ -1,6 +1,149 @@
 import styled from 'styled-components';
 
 export const StyledSection = styled.section`
+  padding-top: 70px;
+  padding-bottom: 70px;
   background-color: #222;
   color: #fff;
+
+  h1 {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 65px;
+    line-height: 70px;
+    font-weight: 600;
+    text-transform: uppercase;
+    color: #fff;
+    letter-spacing: 4px;
+  }
+
+  article {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    section {
+      font-family: 'Source Sans Pro', sans-serif;
+      font-size: 36px;
+      line-height: 44px;
+      font-weight: 400;
+      color: #fff;
+      letter-spacing: 1px;
+      z-index: 1;
+
+      p {
+        margin-bottom: 1rem;
+
+        a {
+          font-size: 36px;
+          line-height: 44px;
+          font-weight: 400;
+          letter-spacing: 1px;
+          z-index: 5;
+          text-transform: uppercase;
+          font-style: italic;
+          font-weight: 600;
+          text-decoration: none;
+          color: #fff;
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
+
+    img {
+      position: relative;
+      left: -115px;
+    }
+  }
+`;
+
+export const Container = styled.div`
+  width: 66.66%;
+  position: relative;
+  margin: 80px auto 0;
+
+  > p {
+    margin-bottom: 20px;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 23px;
+    letter-spacing: 0.9px;
+    color: #fff;
+    text-transform: uppercase;
+  }
+
+  .swiper-container {
+    width: 100%;
+
+    .swiper-slide {
+      strong {
+        font-family: 'Source Sans Pro', sans-serif;
+        font-size: 34px;
+        line-height: 40px;
+        font-weight: 600;
+        font-style: italic;
+        letter-spacing: 2px;
+        text-transform: uppercase;
+        color: #ffffff;
+      }
+      img {
+        max-width: 100%;
+        border-radius: 10px;
+        padding: 8px;
+        opacity: 0.5;
+      }
+    }
+
+    .swiper-slide.swiper-slide-active img {
+      opacity: 1;
+      border: 1px solid #fff;
+    }
+  }
+
+  footer {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    margin-top: 20px;
+
+    article {
+      display: block;
+
+      p {
+        font-family: 'Source Sans Pro', sans-serif;
+        font-size: 18px;
+        line-height: 23px;
+        font-weight: 400;
+        color: #fff;
+        letter-spacing: 0.9px;
+        text-transform: uppercase;
+      }
+    }
+
+    .actions {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+
+      .swiper-button-next,
+      .swiper-button-prev {
+        width: auto;
+        position: static;
+        background-color: transparent;
+        border: none;
+
+        &::after {
+          content: unset;
+        }
+      }
+
+      svg {
+        height: 60px;
+        width: 60px;
+      }
+    }
+  }
 `;
